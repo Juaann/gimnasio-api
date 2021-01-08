5dd2446d5421bbeb05af7df313929cd5e15985cc
JuanitoHe <jhernandezv1402@gmail.com>
2021-01-08 12:03:51 -0600

    Añadi delete y put

--- server.js
+++ server.js
@@ -8,5 +8,13 @@ app.get('/user', function (req, res) {
 app.post('/user', function (req, res) {
   res.send('He creado un user XOXO')
 })
+
+app.put('/user', function (req, res) {
+  res.send('Estos son los users put')
+})
+
+app.delete('/user', function (req, res) {
+  res.send('He creado un user delete XOXO')
+})
  
 app.listen(3000)
